20b242e043dd49f0842de051ee5265613ab18215
Emmanuel Pilande <epilande@gmail.com>
2019-07-26 02:28:52 -0700

    :sparkles: Set up useAuth hook

---
+++ gatsby-theme-auth0/src/hooks/useAuth.ts
@@ -0,0 +1,40 @@
+import * as React from "react";
+import auth from "../auth/service";
+
+const useAuth = () => {
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [isLoggedIn, setIsLoggedIn] = React.useState(auth.isAuthenticated());
+  const [profile, setProfile] = React.useState(auth.getUserProfile());
+
+  React.useEffect(() => {
+    // Override `sessionStateCallback` in auth service
+    auth.sessionStateCallback = state => {
+      setIsLoggedIn(state.isLoggedIn);
+    };
+
+    (async () => {
+      await auth.checkSession();
+      try {
+        const user = auth.getUserProfile();
+        setProfile(user);
+      } catch (error) {
+        console.log(`Error: ${error}`);
+      }
+
+      setIsLoading(false);
+    })();
+
+    return () => {
+      // Clean up sessionStateCallback
+      auth.sessionStateCallback = () => {};
+    };
+  }, []);
+
+  return {
+    isLoading,
+    isLoggedIn,
+    profile,
+  };
+};
+
+export default useAuth;
