fd1b5e8613e0d96c97659f4b6301e492096e15f0
Emmanuel Pilande <epilande@gmail.com>
2019-07-26 02:39:12 -0700

    :bathtub: Clean up gatsby-config

--- gatsby-theme-auth0/gatsby-config.js
+++ gatsby-theme-auth0/gatsby-config.js
@@ -1,15 +1,11 @@
-module.exports = options => {
-  console.log("options: ", options);
-
-  return {
-    plugins: [
-      "gatsby-plugin-typescript",
-      {
-        resolve: "gatsby-plugin-page-creator",
-        options: {
-          path: `${__dirname}/src/pages`,
-        },
+module.exports = () => ({
+  plugins: [
+    "gatsby-plugin-typescript",
+    {
+      resolve: "gatsby-plugin-page-creator",
+      options: {
+        path: `${__dirname}/src/pages`,
       },
-    ],
-  };
-};
+    },
+  ],
+});
